44a61b4f0a78d743239fe2cefdd611c3b7df31ca
Esther Kim <ekim1009@gmail.com>
2022-09-22 13:23:46 -0400

    refactored the winning combinations

--- lesson_2/rock_paper_scissor/rpsls_game.rb
+++ lesson_2/rock_paper_scissor/rpsls_game.rb
@@ -1,6 +1,13 @@
 require 'yaml'
 MESSAGES = YAML.load_file('rpsls_game_messages.yml')
 VALID_OPTIONS = %w(r p sc l sp)
+WINNING_COMBO = {
+  r: ['sc', 'l'],
+  p: ['r', 'sp'],
+  sc: ['p', 'l'],
+  l: ['sp', 'p'],
+  sp: ['r', 'sc']
+}
 
 def prompt(message)
   puts("=> #{message}")
@@ -14,28 +21,16 @@ def operation_to_message(choice)
            'paper'
          when 'sc'
            'scissor'
-         when ' l'
+         when 'l'
            'lizard'
          when 'sp'
            'spock'
          end
 end
 
-def winner(first, second)
-    (first = 'r' && second == 'sc') ||
-    (first = 'r' && second == 'l') ||
-    (first = 'p' && second == 'r') ||
-    (first == 'p' && second == 'sp') ||
-    (first == 'sc' && second == 'p') ||
-    (first == 'sc' && second == 'l') ||
-    (first == 'l' && second == 'sp') ||
-    (first == 'l' && second == 'p') ||
-    (first == 'sp' && second == 'r') ||
-    (first == 'sp' && second == 'sc')
-end
-
 prompt("Welcome to a variation of Rock Paper Scissor")
 prompt("Rock Paper Scissor Lizard Spock")
+prompt("First player to reach 3 points wins")
 puts("________________")
 puts("\n")
 
@@ -56,65 +51,74 @@ end
 prompt("Welcome, #{name}")
 puts("\n")
 
-user_choice = ''
-player_score = 0
-computer_score = 0
-round = 0
 loop do
+  user_choice = ''
+  player_score = 0
+  computer_score = 0
+  current_computer_score = 0
+  current_player_score = 0
+
   loop do
-    # ask user to pick their choice of -rock -paper -scissor - lizard - spock
-    # allow the user to make choices (r, p, sc, l, sp)
-    choice_prompt = <<-MSG
-      what would you like to play?
-      r) rock
-      sc) scissor
-      p) paper
-      l) lizard
-      sp) spock
-      MSG
+    loop do
+      # ask user to pick their choice of -rock -paper -scissor - lizard - spock
+      # allow the user to make choices (r, p, sc, l, sp)
+      choice_prompt = <<-MSG
+        what would you like to play?
+        r) rock
+        sc) scissor
+        p) paper
+        l) lizard
+        sp) spock
+        MSG
 
-      prompt(choice_prompt)
-      user_choice = gets.chomp.strip
+        prompt(choice_prompt)
+        user_choice = gets.chomp.strip
 
-    # make sure their input is a valid option
-    if 
-      VALID_OPTIONS.include?(user_choice)
-      break
-    else
-      prompt("Please make a valid selection")
+      # make sure their input is a valid option
+      if 
+        VALID_OPTIONS.include?(user_choice)
+        break
+      else
+        prompt("Please make a valid selection")
+      end
     end
+
+    # have the computer make a random selection
+    computer_choice = VALID_OPTIONS.sample
+    prompt("You chose #{operation_to_message(user_choice)} and computer chose #{operation_to_message(computer_choice)}")
+
+      # make a program to see which is the winner
+      # winner gets a point per round
+      if
+        user_choice == computer_choice
+        prompt("It's a tie!")
+      elsif
+        WINNING_COMBO.has_key?(user_choice.to_sym) && WINNING_COMBO[user_choice.to_sym].include?(computer_choice)
+        prompt("You win this round!")
+        current_player_score = (player_score += 1)
+      else
+        WINNING_COMBO.has_key?(computer_choice.to_sym) && WINNING_COMBO[computer_choice.to_sym].include?(user_choice)
+        prompt("You lose this round!")
+        current_computer_score = (computer_score += 1)
+      end
+
+      prompt("Your score is #{current_player_score} and the computer score is #{current_computer_score}")
+      puts("\n")
+
+      # once user or computer get 3 point, game is over
+      break if current_player_score == 3 || current_computer_score == 3
+  end
+  # winner is announced
+  if player_score > computer_score
+    prompt("Congratulations!  You have won the game!")
+  else
+    prompt("You lost the game.  Better luck next time!")
   end
 
-  # have the computer make a random selection
-  computer_choice = VALID_OPTIONS.sample
-  prompt("You chose #{operation_to_message(user_choice)} and Computer chose #{operation_to_message(computer_choice)}")
-
-    # make a program to see which is the winner
-    # winner gets a point per round
-    if
-      winner(user_choice, computer_choice)
-      prompt("You win this round!")
-      player_score += 1
-    else
-      prompt("You lose this round")
-      computer_score += 1
-    end
-
-    # once user or computer get 3 point, game is over
-    round += 1
-    break if round == 3
-
-    # winner is announced
-    if player_score > computer_score
-      prompt("Comgratulations!  You have won the game!")
-    else
-      prompt("You lost the game.  Better luck next time!")
-    end
-
   # would user like to play again?
-  prompt("Would you like to play again?")
+  prompt("Would you like to play again? (Y to play again)")
   play_again = gets.chomp.strip.downcase
-  
+
   break unless play_again == 'y'
 end
 
